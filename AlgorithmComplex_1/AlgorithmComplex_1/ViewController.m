c258e51079dee8eb8293a1ad737fd6a5e7756bef
陈双 <chenshuang@banggood.com>
2019-04-24 09:32:32 +0800

    求时间复杂度例子
    求时间复杂度例子

--- AlgorithmComplex_1/AlgorithmComplex_1/ViewController.m
+++ AlgorithmComplex_1/AlgorithmComplex_1/ViewController.m
@@ -18,16 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    int n = 44;
-    // fib1
-    [TimeTool calculateTimeWithTitle:@"fib1" operationBlock:^{
-        [self fib1:n];
-    }];
-    
-    // fib2
-    [TimeTool calculateTimeWithTitle:@"fib2" operationBlock:^{
-        [self fib2:n];
-    }];
+    // fib test
+//    [self fibTest];
 }
 
 // 计算a和b的和
@@ -50,6 +42,19 @@
 }
 
 #pragma mark - 斐波那契数
+
+- (void)fibTest {
+    int n = 44;
+    // fib1
+    [TimeTool calculateTimeWithTitle:@"fib1" operationBlock:^{
+        [self fib1:n];
+    }];
+    
+    // fib2
+    [TimeTool calculateTimeWithTitle:@"fib2" operationBlock:^{
+        [self fib2:n];
+    }];
+}
 
 // 递归
 - (int)fib1:(int)n {
@@ -75,6 +80,89 @@
     }
     
     return second;
+}
+
+#pragma mark - 时间复杂度
+
+// O(1)
+- (void)test1:(int)n {
+    if (n > 10) {
+        NSLog(@"n > 10");
+    } else if (n > 5) { // 2
+        NSLog(@"n > 5");
+    } else {
+        NSLog(@"n <= 5");
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        NSLog(@"test1");
+    }
+}
+
+// O(n)
+- (void)test2:(int)n {
+    // 1 + 3n (指令执行条数)
+    // O(n)
+    for (int i = 0; i < n; i++) {
+        NSLog(@"test");
+    }
+}
+
+// O(n^2)
+- (void)test3:(int)n {
+    // 1 + 2n + n * (1 + 3n) (指令执行条数)
+    // 1 + 2n + n + 3n^2
+    // 3n^2 + 3n + 1
+    // O(n^2)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            NSLog(@"test");
+        }
+    }
+}
+
+// O(n)
+- (void)test4:(int)n {
+    // 1 + 2n + n * (1 + 45) (指令执行条数)
+    // 1 + 2n + 46n
+    // 48n + 1
+    // O(n)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 15; j++) {
+            NSLog(@"test");
+        }
+    }
+}
+
+// O(logn)
+- (void)test5:(int)n {
+    // 执行次数 = log2(n)
+    // O(logn)
+    while ((n = n / 2) > 0) {
+        NSLog(@"test");
+    }
+}
+
+// O(logn)
+- (void)test6:(int)n {
+    // log5(n)
+    // O(logn)
+    while ((n = n / 5) > 0) {
+        NSLog(@"test");
+    }
+}
+
+// O(nlogn)
+- (void)test7:(int)n {
+    // 1 + 2*log2(n) + log2(n) * (1 + 3n)
+    // 1 + 3*log2(n) + 3 * nlog2(n)
+    // O(nlogn)
+    for (int i = 1; i < n; i = i * 2) {
+        // 1 + 3n
+        for (int j = 0; j < n; j++) {
+            NSLog(@"test");
+        }
+    }
 }
 
 @end
